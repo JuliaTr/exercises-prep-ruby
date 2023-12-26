0908bc14af1408fcb73d9f46ff3d9fe72ea04c67
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2023-12-26 12:40:00 -0600

    Add the_basics_3.rb file

---
+++ the_basics/the_basics_3.rb
@@ -0,0 +1,8 @@
+films = { "It's a Wonderful Life": 1946, 
+          "Gremlins": 1984, 
+          "Home Alone": 1990, 
+          "A Christmas Story": 1983 }
+
+films.each do |film, year|
+  puts year
+end
