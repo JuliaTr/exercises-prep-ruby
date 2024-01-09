35645e2a9fe7c3cc857a0d96f28d918f2e652acc
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-01-09 14:21:55 -0600

    Add methods_4.rb

---
+++ methods/methods_4.rb
@@ -0,0 +1,9 @@
+def scream(words)
+  words = words + "!!!!"
+  return
+  puts words
+end
+
+scream("Yippeee")
+
+# The code above doesn't print anything to the screen. Invoking a method with an argument 'scream("Yippee")' returns nil.
