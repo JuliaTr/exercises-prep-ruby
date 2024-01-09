ee4acbc4639f55d72105b9b3a3d35d4df6a0b2e6
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-01-09 10:31:28 -0600

    Add methods_2.rb file

---
+++ methods/methods_2.rb
@@ -0,0 +1,21 @@
+# What value does each expression return?
+# Expression 1:
+x = 2
+# Return: 2
+
+# Expression 2:
+puts x = 2
+# Return: nil
+
+# Expression 3:
+p name = "Joe"
+# Return: "Joe"
+
+# Expression 4:
+four = "four"
+# Return: "four"
+
+# Expression 5:
+print something ="nothing"
+# Return: nil
+
