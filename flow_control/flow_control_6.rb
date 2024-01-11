4a4a5d6599ea9fc0f11df33fa5e25b2a78f69699
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-01-11 15:41:56 -0600

    Add flow_control_6.rb file

---
+++ flow_control/flow_control_6.rb
@@ -0,0 +1,6 @@
+(32 * 4) >= "129"   # Raise an error 'comparison of Integer with String failed'
+847 == '847'        # Return: false
+'847' < '846'       # Return: false
+'847' > '846'       # Return: true
+'847' > '8478'      # Return: false
+'847' < '8478'      # Return: true
