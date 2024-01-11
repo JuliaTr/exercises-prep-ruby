a2525cd2c00819b42beb681001ea91f49d3a1222
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-01-11 11:27:17 -0600

    Add flow_control_1.rb file

---
+++ flow_control/flow_control_1.rb
@@ -0,0 +1,5 @@
+(32 * 4) >= 129                                               # Return: false
+false != !true                                                # Return: false
+true == 4                                                     # Return: false
+false == (847 == '847')                                       # Return: true
+(!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false   # Return: true
