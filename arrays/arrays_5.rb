2ef18baefbfd6813ef97fcb68c9adc30c7ee90d5
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-01-17 16:27:25 -0600

    Add array_5.rb file

---
+++ arrays/arrays_5.rb
@@ -0,0 +1,11 @@
+string = "Welcome to America!"
+a = string[6]                
+# Return: the second "e" in "Welcome" word.
+b = string[11]
+# Return: the first "A" in "America" word.
+c = string[19]
+# Return: nil
+
+p a
+p b
+p c
