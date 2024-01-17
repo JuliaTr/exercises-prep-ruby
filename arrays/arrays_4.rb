7d83fc7a7eaeec620e25cee40fbcaa3b46bb12e4
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-01-17 16:14:59 -0600

    Add arrays_4.rb file

---
+++ arrays/arrays_4.rb
@@ -0,0 +1,7 @@
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+arr.index(5)
+# Return: 3
+arr.index[5]
+# Return: NoMethodError: undefined method `[]' for #<Enumerator: [15, 7, 18, 5, 12, 8, 5, 1]:index> 
+arr[5]
+# Return: 8
