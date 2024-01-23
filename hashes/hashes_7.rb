130122466e8eb5029c279924112f65952c993d06
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-01-23 15:40:36 -0600

    Add hashes_7.rb file

---
+++ hashes/hashes_7.rb
@@ -0,0 +1,5 @@
+=begin
+If we see 'NoMethodError: undentified method `keys' for Array',
+there's no method called 'keys' for Array objects.
+There are no keys in an Array. We use 'keys' method for Hashes.
+=end
