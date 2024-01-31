8d98ce2dbc35b94e3a94381a2c3124d3592b973b
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-01-31 16:10:09 -0600

    Add more_stuff_3.rb file

---
+++ more_stuff/more_stuff_3.rb
@@ -0,0 +1,5 @@
+=begin
+Exception handling is a structure in a piece of code which identifies an error in the program.
+It changes the flow of control without exiting the program entirely. 
+Also, an excepion handling may include logging the error, which has happened.
+=end
