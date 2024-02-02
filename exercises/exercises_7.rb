ee018bdcc79ddf54b963942bdf8bfd9b122aecb1
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-02-02 10:05:48 -0600

    Add exercises_7.rb file

---
+++ exercises/exercises_7.rb
@@ -0,0 +1,13 @@
+# Array is a collection of ordered list of literals in brackets '[]'. We iterate using index of each liteeral.
+array = [1, 'hi, there', nil, [7, 8], {parrot: 'red', panthera: 'black'}, 7.08, false]
+
+# Hash is a collection of key/pair data in curly braces '{}'.
+hash = {parrot: 'red', panthera: 'black'}
+
+# The difference beween them is that we can refence by key in a hash.
+puts hash[:parrot]
+puts array[0]
+
+
+
+
