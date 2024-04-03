3ed59d818842fce71aa2d3b1e5b146b9d43f8e4f
Yuliia Tryhubniak <juliatrygybniak@gmail.com>
2024-04-03 14:01:23 -0500

    Changed if statement for readability

--- flow_control/flow_control_2.rb
+++ flow_control/flow_control_2.rb
@@ -2,7 +2,11 @@ puts "Type something longer than 10 characters."
 words = gets.chomp
 
 def converted(words)
-  new_words = words.length > 10 ? words.upcase! : "Please type something longer than 10 characters"
+  if words.length > 10 
+    words.upcase
+  else
+    "Please type something longer than 10 characters"
+  end
 end
 
 puts converted(words)
